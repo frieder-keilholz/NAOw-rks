955ad0c93e65d9c5c3aa1ad93420f1648989fdbc
frieder-keilholz <fgk1999@outlook.de>
2020-06-13 21:26:25 +0200

    -.

--- Webinterface/modules.js
+++ Webinterface/modules.js
@@ -56,12 +56,12 @@ function addModuleCard(moduleJSON){
     cardBodyBtnDetails.onclick = "showDetails("+moduleJSON.module_id+")";
     cardBodyBtnDetails.classList = "btn btn-primary";
     cardBodyBtnDetails.innerHTML = "Details";
-    card.appendChild(cardBodyBtnDetails);
+    cardBody.appendChild(cardBodyBtnDetails);
     var cardBodyBtnExport = document.createElement("button");
     cardBodyBtnExport.onclick = "showExport("+moduleJSON.module_id+")";
     cardBodyBtnExport.classList = "btn btn-primary";
     cardBodyBtnExport.innerHTML = "Export";
-    card.appendChild(cardBodyBtnExport);
+    cardBody.appendChild(cardBodyBtnExport);
     card.appendChild(cardBody);
 
     var boxCard = document.createElement("div");
