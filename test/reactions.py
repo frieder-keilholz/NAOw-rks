c28e7419bcec5a90b19655a522b48c67883229f1
jure5622 <jure5622@th-wildau.de>
2020-06-26 02:33:06 +0200

    ...

--- test/reactions.py
+++ test/reactions.py
@@ -43,16 +43,18 @@ def initialize():
 def right_answer(right_answer):
     initialize()
     anims.say("^start(Sit/Emotions/Positive/Happy_2)", configuration)
+    time.sleep(1)
     tts.say(right_answer)
-    time.sleep(6)
+    time.sleep(2)
     put_to_rest()
 
 
 def wrong_answer(wrong_answer):
     initialize()
     anims.say("^start(Sit/Emotions/Negative/Frustrated_1) ", configuration)
+    time.sleep(1)
     tts.say(wrong_answer)
-    time.sleep(6)
+    time.sleep(2)
     put_to_rest()
 
 
